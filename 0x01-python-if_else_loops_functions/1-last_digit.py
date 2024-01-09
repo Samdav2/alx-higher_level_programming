1e7e6e8f357c8af0a3fbb1d0f590b138ad64b46e
Samdav2 <dawodujohn1@gmail.com>
2024-01-09 05:17:32 -0800

    making changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-convert = chr(number)
-print(f"{number[-1]}")
+whole = str(number)
+last = whole[-1]
+print(f"{whole}last{last}")
