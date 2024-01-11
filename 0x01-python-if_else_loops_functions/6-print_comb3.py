85455e1f15e36468be7fbaabaf070b2a2d7c7cd7
Samdav2 <dawodujohn1@gmail.com>
2024-01-10 22:12:10 -0800

    c program that print numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for w in range (i + 1, 10):
+    for w in range(i + 1, 10):
         if i == 8:
             print("{}{}" .format(i, w))
         else:
