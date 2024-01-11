63adcaa2c613d69595c786787f9f14c11c678f2f
Samdav2 <dawodujohn1@gmail.com>
2024-01-11 17:09:53 +0100

    Python script that print FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(101):
-        if i % 3 == 0:
+        if i > 0 and i % 3 == 0:
             print("Fizz", end=" ")
-        elif i % 5 == 0:
+        elif i > 0 and i % 5 == 0:
             print("Buzz", end=" ")
-        elif i % 15 == 0:
+        elif i > 0 and i % 15 == 0:
             print("FizzBuzz", end=" ")
         else:
             print("{}" .format(i), end=" ")
-    print("")
