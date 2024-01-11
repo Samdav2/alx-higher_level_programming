4b2786780cd55752354f957e9d0a4162e3632308
Samdav2 <dawodujohn1@gmail.com>
2024-01-11 14:36:33 +0100

    Fixing bug

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(ord(str)):
-        if i not in chr(i) == " ":
+    for i in range(str):
+        if i not in (i) == " ":
+            i = ord(i)
             i -= 32
             i = chr(i)
             print("{i}" .format(i))
