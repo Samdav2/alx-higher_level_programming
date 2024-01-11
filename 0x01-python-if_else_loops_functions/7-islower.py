cf3cca00e0f0b24b636054b7c8e91e2937fd0220
Samdav2 <dawodujohn1@gmail.com>
2024-01-11 08:26:51 +0100

    C program that check if a program is lower or upper case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z') + 1):
-        if c == i:
+        if i == c:
             return True
         else:
             return False
