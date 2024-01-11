8bc282f47c5f868bc6b18716e737a03ad6537143
Samdav2 <dawodujohn1@gmail.com>
2024-01-11 12:41:24 +0100

    Pyhton script that coverts lower case to upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(ord(str)):
+        if i not in chr(i) == " ":
+            ord(i) -= 32
+            i = chr(i)
+            print("{i}" .format(i))
+        else:
+            print("{i}" .format(chr(i))
