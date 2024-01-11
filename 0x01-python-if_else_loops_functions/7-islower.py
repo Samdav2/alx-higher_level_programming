dc67d6c8b430381a4981892d9582ba6b8dcc8918
Samdav2 <dawodujohn1@gmail.com>
2024-01-11 12:20:37 +0100

    Fixing bug

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in range(ord('a'), ord('z') + 1):
-        if i == c:
+        if chr(i) == c:
             return True
         else:
             return False
