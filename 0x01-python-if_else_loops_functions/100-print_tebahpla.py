6030a73136ea640be231bd8e8f91cfd19ba807cd
Samdav2 <dawodujohn1@gmail.com>
2024-01-12 01:37:17 +0100

    print letters in different ways

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    k = 1
+    k += 1
+    if k % 2 == 0:
+        i -= 32
+        print("{}" .format(chr(i), end="")
+    else:
+        print("{}" .format(chr(i), end="")
