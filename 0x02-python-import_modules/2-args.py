0508d9a6bbf33c284e803938994c6ec947d0913a
Samdav2 <dawodujohn1@gmail.com>
2024-01-13 14:54:52 +0100

    Working on arguement

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+if __name__ = "__main__":
+    a = len(argv)
+    print("{} arguement:" .format(a))
+    k = 0
+    for i int sys.argv[]:
+        k += 1
+        print("{}: {}" .format(a, i))
