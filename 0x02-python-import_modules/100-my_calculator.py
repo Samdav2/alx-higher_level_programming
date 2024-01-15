9eee7eb335f50dd7c8a21dbd997870b32757c0d8
Samdav2 <dawodujohn1@gmail.com>
2024-01-15 12:33:47 +0100

    Fixing Bug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argc = len(argv) - 1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit 1
+        exit(1)
     a = int(argv[1])
     b = int (argc[3])
     for i in argv:
@@ -19,3 +19,4 @@ if __name__ == "__main__":
             print("{} {} {} = {}" .format(a, b, i, mul(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
