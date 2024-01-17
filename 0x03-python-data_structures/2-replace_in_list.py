7f54238fe50952d14414798b7b3679ab48312dd7
Samdav2 <dawodujohn1@gmail.com>
2024-01-17 12:44:19 +0100

    Fixing bug

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,4 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > a:
         return my_list
     else:
-        return my_list[idx] = element
+        my_list.insert(idx, element)
+        return my_list
