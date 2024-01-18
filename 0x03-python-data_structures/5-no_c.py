ce98bba4cb7c001b2ecd532d8c3832ede9b2889e
Samdav2 <dawodujohn1@gmail.com>
2024-01-18 03:27:15 -0800

    Fixing Bug

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,9 @@ def no_c(my_string):
     a = len(my_string)
     new_string = []
     for i in range(a):
-        if my_string[i]== "c" or my_string[i] == "C":
+        if my_string[i] == "c" or my_string[i] == "C":
             continue
         else:
-           new_string.append(my_string[i])
+            new_string.append(my_string[i])
     new2_string = ''.join(new_string)
     return new2_string
