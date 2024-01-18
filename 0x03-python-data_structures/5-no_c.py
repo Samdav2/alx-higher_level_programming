c8bd12a3826fd83a6821cfbf134cc24460972581
Samdav2 <dawodujohn1@gmail.com>
2024-01-18 12:23:19 +0100

    fixing Bug For Pycodestyles

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    j = 0
     a = len(my_string)
     new_string = []
     for i in range(a):
-        j += 1
         if my_string[i]== "c" or my_string[i] == "C":
             continue
         else:
