3a8f91b0302349ac8010140c0d6157e39b6e7d67
Samdav2 <dawodujohn1@gmail.com>
2024-01-22 14:52:18 +0100

    Python script that adds two intigers

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    add = []
+    if len(tuple_b) < 1:
+        add[0] = tuple_a[0] + tuple_b[0]
+        add[1] = tuple_a[1] + 0
+    else:
+        add[0] = tuple_a[0] + tuple_b[0]
+        add[1] = tuple_a[1] + tuple_b[1]
+    return add
