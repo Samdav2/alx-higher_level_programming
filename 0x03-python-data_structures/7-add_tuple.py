35a9899c37e4a2f585135cf688dd392d17ca69e8
Samdav2 <dawodujohn1@gmail.com>
2024-01-22 15:29:11 +0100

    Fixing Bug

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 1:
-        add = tuple(a + b for a, b in zip(tuple_a, tuple_b)
+        add = tuple(a + b for a, b in zip(tuple_a, tuple_b))
     else:
-        add = tuple(a + b for a, b in zip(tuple_a, tuple_b)
+        add = tuple(a + b for a, b in zip(tuple_a, tuple_b))
     return add
