9c6711469c870abc3fd31c1c13a64771ff2109ef
Samdav2 <dawodujohn1@gmail.com>
2024-01-23 12:24:20 -0800

    Fixing Bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,8 @@
 def multiple_returns(sentence):
     a = len(sentence)
     word = sentence
-    return(a, word[0])
+    if word == None:
+        word[0] = None
+        return(a, word[0])
+    else:
+        return(a, word[0])
