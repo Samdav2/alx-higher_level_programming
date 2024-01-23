a4cefa0e7b91d5fc8229145e9bc172cc86af7e14
Samdav2 <dawodujohn1@gmail.com>
2024-01-23 21:44:44 +0100

    Python script that performs specific function

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    a = len(my_list)
+    j = 0
+    for i in my_list:
+        j += 1
+        if i > my_list[a - j]:
+            break
+            print("{}" .format(i))
+        else:
+            continue 
