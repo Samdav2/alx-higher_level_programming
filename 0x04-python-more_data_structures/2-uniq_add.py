a2e3103d82314202ffcc1b1e14b269c1e5514159
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 20:50:46 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,10 +5,9 @@ def uniq_add(my_list=[]):
     for i in my_list:
         temp = len(new_list)
         for j in range(temp):
-            if i == new_list[j]:
+            new_list.append(i)
+            if i == new_list(j):
                 continue
-            else:
-                new_list.append(i)
     add = 0
     for w in new_list:
         add += w
