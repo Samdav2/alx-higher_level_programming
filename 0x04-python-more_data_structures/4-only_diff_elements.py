612f94657f334a225dc8c287e4f863761bc042a4
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 22:19:18 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,21 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = []
-    equal = []
-    same = []
-    new_set2 = []
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                same.append(i)
-            else:
-                equal.append(i)
-                equal.append(j)
-    new_set = sorted(set(equal))
-    for k in new_set:
-        for s in same:
-            if s == k:
-                continue
-            else:
-                new_set2.append(k)
-    return new_set2
+    return set_1 ^ set_2
