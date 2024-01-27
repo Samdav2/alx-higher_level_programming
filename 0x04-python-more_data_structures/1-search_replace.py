3a6a632d2021095c1f5910e994130b5c906065dd
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 09:32:52 -0800

    Fixing Bug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == search:
-            new_list.append(list(map(lambda x: x = replace, i)))
+            new_list.append(list(map(lambda x: x == replace, i)))
         else:
             new_list.append(i)
     return new_list
