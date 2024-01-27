e489bf11ef458132fde5618c407528674974e423
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 21:20:50 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@ def common_elements(set_1, set_2):
     len1 = len(set_1)
     len2 = len(set_2)
     total = len1 + len2
-    for i in range(total):
-        if set_1{i} == set_2{i}:
-            equal = set_1
+    for i, w in set_1, set_2:
+        if i == w:
+            equal = w
     return equal
