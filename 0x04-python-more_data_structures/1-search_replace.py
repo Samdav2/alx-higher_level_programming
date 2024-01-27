a64f7c8cf5919b9cc15de1b1d9d8e84675664718
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 18:34:47 +0100

    Python script that searches and replace a number

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,8 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if i == search:
-            new_list.append(list(map(lambda x: x == replace, i)))
+            i = search
+            new_list.append(i)
         else:
             new_list.append(i)
     return new_list
