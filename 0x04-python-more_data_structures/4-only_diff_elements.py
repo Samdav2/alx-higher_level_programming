4ba1eec81163fa98e547f63861f1a57608d63131
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 22:00:04 +0100

    Python script that add two set toghter exept the intersecting element

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = []
-    equal = []
-    equal = set_1 + set_2
+    for i in set_1:
+        for j in set_2:
+            equal.append(i)
+            equal.appen(j)
     new_set = sorted(set(equal))
     return new_set
