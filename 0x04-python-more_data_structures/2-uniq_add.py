65613405e9a682f546911890107c02d525f863c0
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 20:45:16 +0100

    Python script that adds in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,12 @@ def uniq_add(my_list=[]):
     index = 0
     new_list = []
     for i in my_list:
-        new_list.append(i)
-        if i == new_list
-            continue
+        temp = len(new_list)
+        for j in range(temp):
+            if i == neww_list[j]:
+                continue
+            else:
+                new_list.append(i)
     add = 0
     for w in new_list:
         add += w
