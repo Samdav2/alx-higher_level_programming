3cfbf84c8815858195fa8985b0f63b3d3b4cdf1d
Samdav2 <dawodujohn1@gmail.com>
2024-01-27 16:10:51 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     return x ** 2
-squared = map(square_matrix_simple, matrix)
-listing = list(squared)
-return listing
+    squared = map(square_matrix_simple, matrix)
+    listing = list(squared)
+    return listing
