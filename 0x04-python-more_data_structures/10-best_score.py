1300e17b500918bcbc3ade56f415f30fb6bdad0a
Samdav2 <dawodujohn1@gmail.com>
2024-01-28 12:58:39 +0100

    Python script that looks for the largest key in a dict

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    a = len(a_dictionary)
+    j = 0
+    for i in a_dictionary:
+        j += 1
+        a = a_dictionary[i]
+        if a < a_dictionary[i] and j != a:
+            continue
+        else:
+            a = a_dictionary[i]
+    return a
