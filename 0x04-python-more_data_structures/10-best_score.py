a26027e24b3552328bf9a87f354d483d75fd7369
Samdav2 <dawodujohn1@gmail.com>
2024-01-28 13:13:35 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,9 +6,9 @@ def best_score(a_dictionary):
     j = 0
     for i in a_dictionary:
         j += 1
-        a = a_dictionary[i]
+        a = i
         if a < a_dictionary[i] and j != b:
             continue
         else:
-            a = a_dictionary[i]
+            a = i
     return a
