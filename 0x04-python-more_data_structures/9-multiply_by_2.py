195b089ca76adeb18a45a5793db492b299d75826
Samdav2 <dawodujohn1@gmail.com>
2024-01-28 11:22:27 +0100

    Fixing Bug

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new = []
     for i in a_dictionary[keys]:
-        new = list(map(lambda x: x ** 2, int(i)))
+        new = list(map(lambda x: x ** 2, a_dictionary[i]))
     return new
