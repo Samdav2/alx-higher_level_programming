80cc901394673a9decd8a0b06d2479507582fbcb
Samdav2 <dawodujohn1@gmail.com>
2024-01-31 17:23:29 +0100

    Handling Error in Python

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    try:
+        for i in range x:
+            j += 1
+            return my_list[i]
+    except IndexError:
+        x = j
+        for k in range x:
+            return my_list[k]
