54e5aee6ca95a235cb8b71d4a1bf746912626b23
Samdav2 <dawodujohn1@gmail.com>
2024-02-02 15:47:42 +0100

    Fixing Bug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        a / b
+        c = a / b
     except ZeroDivisionError:
         return None
     else:
         return a / b
     finally:
-        c = a / b
         print("Inside Result: {}".format(c))
