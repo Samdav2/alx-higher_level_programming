dfbd8d7d24ee9c9a3dc8ecdeeafbc47bbcdb222c
Samdav2 <dawodujohn1@gmail.com>
2024-02-02 11:17:41 +0100

    Python script that print element in a list and handles errors

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in my_list:
+        try:
+            print("{:d}".format(my_list[x]))
+            print("{:d}".format(my_list[i]))
+        except (IndexError, TypeError, ValueError):
+            print(end="")
+        else:
+            j += 1
+            print("{:d}".format(my_list[i]), end="")
+    print()
+    return j
+
