969789977039376d4156f03b507c4ef0fa1a059d
Samdav2 <dawodujohn1@gmail.com>
2024-02-02 10:58:50 +0100

    Fiximg Bug

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     int_test = True
     try:
-        print("{:d}" .format(value))
+        print("{:d}".format(value))
     except (ValueError, TypeError):
         int_test = False
     return int_test
