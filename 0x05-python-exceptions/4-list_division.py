2b7548a9e4b3bd0556543be6810dddcf79325fc3
Samdav2 <dawodujohn1@gmail.com>
2024-02-02 21:42:43 +0100

    Python script that return the division of a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_lenght):
+        try:
+            map(lambda x, y: x / y, my_list_1[i], my_list_2[i])
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+        except (ValueError, TypeError):
+            print("wrong type")
+        else:
+            new_list = list(map(lambda x, y: x / y, my_list[i], my_list_2[i]))
+        finally:
+            return new_list
