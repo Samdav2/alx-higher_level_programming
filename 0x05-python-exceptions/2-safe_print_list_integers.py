e67b5f6b152c2fa4f78407a5a8e60e4951324b4b
Samdav2 <dawodujohn1@gmail.com>
2024-02-02 11:25:25 +0100

    Fixing Bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     for i in my_list:
         try:
-            print("{:d}".format(my_list[x]))
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[x]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except (IndexError, TypeError, ValueError):
             print(end="")
         else:
             j += 1
-            print("{:d}".format(my_list[i]), end="")
     print()
     return j
 
