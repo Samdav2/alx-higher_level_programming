a46a18e82cafedb9cc6c278471cb3b74dc668de0
SamDav2 <dawodujohn1@gmail.com>
2024-02-08 00:41:46 +0100

    Fixing Bug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ A class that defines squres with plrivate attribute"""
 
-class Square(self):
+class Square(self, size):
     """ A class that create an instance attribut"""
     self.__size = None
