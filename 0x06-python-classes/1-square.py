a5face9c40f111d87f4c047406cf7f4cc00f0393
SamDav2 <dawodujohn1@gmail.com>
2024-02-08 00:34:57 +0100

    Ptrhon script that create a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A class that defines squres with plrivate attribute"""
+
+class Square:
+    """ A class that create an instance attribut"""
+    def __init__(self):
+        """ A function that automatically set the value of an attribut"""
+        self.__size = None
