f640d461e353f5fc22b16c4d77465a1ede72e801
SamDav2 <dawodujohn1@gmail.com>
2024-02-08 21:31:51 +0100

    Fixing Bug

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """ The class name square that validate the size"""
     def __init__(self, size=0):
         """ This automatically Initialize the size and contetn"""
-        square.size = size
+        self.size = size
         if not int(size):
             raise TypeError("size must be an integer")
         elif size < 0:
