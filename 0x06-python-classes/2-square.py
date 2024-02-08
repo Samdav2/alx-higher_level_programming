ba2d414a3a35c40a9cb953d29ee522c26dc2438f
SamDav2 <dawodujohn1@gmail.com>
2024-02-08 21:23:58 +0100

    Python class script that validate the value of a size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is a code that validates the size of a Class"""
+
+
+class Square:
+    """ The class name square that validate the size"""
+    def __init__(self, size=0):
+        """ This automatically Initialize the size and contetn"""
+        square.size = size
+        if not int(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        pass
