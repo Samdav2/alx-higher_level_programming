b923bb38e519b8191270ef3fe72196bdd5a89728
SamDav2 <dawodujohn1@gmail.com>
2024-02-10 14:37:39 +0100

    Fixing Bug

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,7 +22,7 @@ class Square:
         """ function that set the value"""
         if (isinstance(value, int) is False):
             raise TypeError("size must be an integer")
-        elif size< 0:
+        elif value < 0:
             raise ValueError("Size must be >= 0")
         else:
             self.size = value
