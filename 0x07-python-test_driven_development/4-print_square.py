26f0289c3873082d274a064c9dfcf87d5467d903
SamDav2 <dawodujohn1@gmail.com>
2024-02-15 21:50:23 +0100

    Python script thag print sqaures

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This is a python script tha prints square"""
+
+
+def print_square(size):
+    """ This is a function that print square"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if isinstance(size, False) is True ans size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(0, size):
+        for j in range(i):
+            print("#", end="")
+        print()
+    print()
