bfd9f52b6262c85d167a71642b3dfc3637723a3a
SamDav2 <dawodujohn1@gmail.com>
2024-02-16 14:24:30 +0100

    Python script that refomat string

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This is a python script that reformat a test"""
+
+
+def text_indentation(text):
+    """ This is a function that split a sentence"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if i == "?" or i == "." or i == "," or i == ":":
+            print()
+            print()
+        else:
+            print("{}".format(i), end="")
