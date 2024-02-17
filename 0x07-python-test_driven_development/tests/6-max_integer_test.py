7934179747aad2c5c04ed761fe18261ed3369349
SamDav2 <dawodujohn1@gmail.com>
2024-02-17 16:41:28 +0100

    Fixing Bug

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """
-    This is a python script and text file
+    This is a python script and test a python file
 """
 import unittest
-def max_integer(list=[]):
-    """ This is a function that find
-        the number with the largest 
-        value in a list
-    """
 
-    new = []
+max_integer = __import__('6-max_integer').max_integer
 
-    new = sort(list)
-    return new[-1]
+
+class TestingMaxInteger(unittest.TestCase):
+
+
+    "run the test with python3 -m unittest -v tests.6-max_integer_test"""
+
+    def test_module_doctring(self):
+        moduleDc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(noduleDoc) > 1)
+
+    def test_function_doctstring(self):
+        functionDoc = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(functionDoc) > 1)
+
+    def test_signed_ints_and_floats(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
