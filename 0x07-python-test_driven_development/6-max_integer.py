dec9af69c0a918eb5114a74cae5a28b320009d2e
SamDav2 <dawodujohn1@gmail.com>
2024-02-17 16:42:00 +0100

    Fixing Bug

---
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    This is a python script and text file
+"""
+import unittest
+
+def max_integer(list=[]):
+    """ This is a function that find
+        the number with the largest
+        value in a list
+    """
+
+    new = []
+
+    new = sort(list)
+    return new[-1]
+
