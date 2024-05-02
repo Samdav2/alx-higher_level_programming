c8842f181694c42330a2435d2f684d838e31a406
SamDav2 <dawodujohn1@gmail.com>
2024-05-02 19:25:20 +0100

    python script that create a class of rectangle and solve the area and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Python file that solve the
+area and perimeter of a
+rectangle
+"""
+
+
+class Rectangle:
+    """A class tha defines and solve for rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if width == 0 or height == 0:
+            return 0
+        return (height + width) * 2
