bccf9a1a2b19f8ab2551015a42ea352e523b2398
SamDav2 <dawodujohn1@gmail.com>
2024-05-03 10:34:26 +0100

    Fixing Bug

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.height * self.width
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
