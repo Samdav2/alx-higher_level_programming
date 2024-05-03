0cefb91808ed4c34b1ea0918aef9017b3bf87d1c
SamDav2 <dawodujohn1@gmail.com>
2024-05-03 10:50:58 +0100

    Fixing Bug

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.___width = value
+        self.__width = value
 
     @property
     def height(self):
