b9c9767118594b65f52b2af1e0d056ba3dc6eb60
SamDav2 <dawodujohn1@gmail.com>
2024-05-20 20:45:57 +0100

    Python script return a sort list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is a function inherit from a class"""
+
+
+class MyList(list):
+    """ This is a child class that inherit from the parent"""
+
+    def print_sorted(self):
+        return sort(list)
