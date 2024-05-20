efdcd2be4ca0d4fe03aa32322604490c94c9d172
SamDav2 <dawodujohn1@gmail.com>
2024-05-20 20:17:05 +0100

    Python script that return the directory of a method

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Ths is a class that returns the list of a method"""
+
+
+def lookup(obj):
+    """ Function thatn return a list of methods attributes"""
+
+    return dir(obj)
