cebf2e4ec917e4d2db7917eafc19d3c70fed1e6b
SamDav2 <dawodujohn1@gmail.com>
2024-05-20 20:53:33 +0100

    Fixing Bug

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,8 @@ class MyList(list):
     """ This is a child class that inherit from the parent"""
 
     def print_sorted(self):
-        return sort(list)
+        """ This print a sorted list"""
+
+        list_cp = self.copy()
+        list_cp.sort()
+        print("{}" .format(list_cp))
