8da90d3d1225c09fc559229013499f742ec3846c
SamDav2 <dawodujohn1@gmail.com>
2024-05-24 17:40:31 +0100

    Raising and Eception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is an improved Geometry"""
+
+
+class BaseGeometry:
+    """ This is a class about Geometry"""
+
+    def area(self):
+        """ A function to raise exception"""
+
+        raise("area() is not implemented")
