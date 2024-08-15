6ba0eb5f0f7320bc380c2f8138959038dea129e8
SamDav2 <dawodujohn1@gmail.com>
2024-08-15 20:16:12 +0100

    Fixing Bug

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,14 +4,11 @@
 
 class Student:
     """A class student with public class"""
-    firs_name = NULL
-    last_name = NULL
-    age = NULL
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
-        self.age  = age
+        self.age = age
 
     def to_json(self):
         return self.__dict__
