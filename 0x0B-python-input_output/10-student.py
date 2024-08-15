8df38e24c943e54060059ba1c5153a90c90ae570
SamDav2 <dawodujohn1@gmail.com>
2024-08-15 20:34:53 +0100

    Python script that return a script

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This is a python function that returns python keys"""
+
+
+def Student:
+    """ This is a class that defines public variables"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs != None:
+            return self.__dict__[attrs]
+        else:
+            return self.__dict__
+
+
+
