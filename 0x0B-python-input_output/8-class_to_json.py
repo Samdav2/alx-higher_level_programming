a3c1fb540d3e8244d9837517665edf03b60ae5cc
SamDav2 <dawodujohn1@gmail.com>
2024-08-15 20:06:10 +0100

    Python class that returns a dictionary of a class

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This function returns the disctionary description"""
+
+
+def class_to_json(obj):
+    """ This returns the dictionary description with simple data structure"""
+    return obj.__dict__
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This is a class of a studen """
+
+
+class Student:
+    """A class student with public class"""
+    firs_name = NULL
+    last_name = NULL
+    age = NULL
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age  = age
+
+    def to_json(self):
+        return Student.__dict__
